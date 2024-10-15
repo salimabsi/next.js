2fcc5970238f2a3ca0383cf537b9dc03bbf68c3c
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-10-15 11:16:39 +0900

    Fix resolving issues

--- turbopack/crates/turbopack-ecmascript/src/references/esm/export.rs
+++ turbopack/crates/turbopack-ecmascript/src/references/esm/export.rs
@@ -23,7 +23,6 @@ use turbopack_core::{
     issue::{analyze::AnalyzeIssue, IssueExt, IssueSeverity, StyledString},
     module::Module,
     reference::ModuleReference,
-    resolve::ModulePart,
 };
 
 use super::base::ReferencedAsset;
@@ -31,7 +30,6 @@ use crate::{
     chunk::{EcmascriptChunkPlaceable, EcmascriptExports},
     code_gen::{CodeGenerateable, CodeGeneration, CodeGenerationHoistedStmt},
     magic_identifier,
-    tree_shake::asset::EcmascriptModulePartAsset,
 };
 
 #[derive(Clone, Hash, Debug, PartialEq, Eq, Serialize, Deserialize, TraceRawVcs)]
